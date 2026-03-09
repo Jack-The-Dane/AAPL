44bd838046860dbeef317d2728460729c292d837
TheJoboReal <kasper.jobo@gmail.com>
2026-03-09 10:51:38 +0100

    button state machine

--- vhdl/pl_side/pl_side.runs/impl_1/runme.sh
+++ vhdl/pl_side/pl_side.runs/impl_1/runme.sh
@@ -38,7 +38,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log button_LED_wrapper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source button_LED_wrapper.tcl -notrace
 
 
